ecf52c32f5c48da6c896c8c50903fddeb64785ea
ashkan <ashkanpower@gmail.com>
2014-08-09 13:20:19 +0430

    fragment login added

--- app/src/main/java/com/pga/project1/fragment/FragmentLogin.java
+++ app/src/main/java/com/pga/project1/fragment/FragmentLogin.java
@@ -39,7 +39,7 @@ public class FragmentLogin extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_main,
+        View view = inflater.inflate(R.layout.fragment_login,
                 container, false);
         return view;
     }
