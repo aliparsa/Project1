057dabdc6dae1510dbb574b0dab8c5482bba8873
Pishgaman-Asia <android@pishgamanasia.com>
2014-08-09 13:31:05 +0430

    Push test Ali

---
+++ app/src/main/java/com/pga/project1/fragment/FragmentProjectTreeView.java
@@ -0,0 +1,131 @@
+package com.pga.project1.fragment;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.pga.project1.R;
+
+public class FragmentProjectTreeView extends Fragment {
+
+//	ImageView ivIcon;
+	TextView txt;
+	ProgressBar prgbar;
+	Button btn;
+//	CustomContentListViewAdapter adapter;
+//	boolean is_data_received = false;
+//
+//	public boolean loaded=false;
+//
+//	ListView lv_cat;
+//	ListView lv_product;
+//
+//	String father;
+
+	// array list to store items recived form activity
+	// ArrayList<TreeMenuItem> itemlist;
+
+//	public static final String IMAGE_RESOURCE_ID = "iconResourceID";
+//	public static final String ITEM_NAME = "itemName";
+
+	// ------------------------------------------------------------------------------------
+	public FragmentProjectTreeView() {
+		
+	}
+
+	// ------------------------------------------------------------------------------------
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_layout_splash, container,
+				false);
+
+		txt = (TextView) view.findViewById(R.id.textView1);
+		prgbar = (ProgressBar) view.findViewById(R.id.progressBar1);
+		btn = (Button) view.findViewById(R.id.button1);
+		
+		getActivity().getActionBar().hide();
+		
+		//CheckIntenetConnection();
+
+        // Call Login Fraqgment
+        Fragment frag = new FragmentLogin();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, frag)
+                .commit();
+
+		return view;
+	}
+	// ------------------------------------------------------------------------------------
+//	public void CheckIntenetConnection(){
+//		txt.setText("بارگذاری...");
+//		prgbar.setVisibility(ProgressBar.VISIBLE);
+//		btn.setVisibility(Button.GONE);
+//
+//		ListView mDrawerList = new ListView(getActivity());
+//
+//		new AsynLoadByTag(getActivity(), "top", null).execute();
+//		new AsynLoadByTag(getActivity(), "new", null).execute();
+//		new AsynLoadByTag(getActivity(), "new", null).execute();
+//
+//		new AsynLoadMainGroups(getActivity(),new CallBackAsync() {
+//
+//			@Override
+//			public void onSuccess(Object result) {
+//				// TODO Auto-generated method stub
+//				IsConnected(true,(CustomDrawerAdapter)result);
+//
+//			}
+//
+//			@Override
+//			public void onError(String errMessage) {
+//				// TODO Auto-generated method stub
+//				IsConnected(false,null);
+//
+//			}
+//		}).execute("");
+//
+//	}
+	// ------------------------------------------------------------------------------------
+//	public void IsConnected(boolean status,CustomDrawerAdapter adapter){
+//		if (status==true){
+//			//TODO Set Adapter to Drawer list view
+//			((MainActivity) getActivity()).initApp(adapter);
+//
+//		}else{
+//			//Toast.makeText(this, "Not Connected", Toast.LENGTH_SHORT).show();
+//			 txt.setText("ارتباط مقدور نیست");
+//			 btn.setText("تلاش مجدد");
+//			 prgbar.setVisibility(ProgressBar.GONE);
+//			 btn.setVisibility(Button.VISIBLE);
+//			 btn.setOnClickListener(new OnClickListener() {
+//
+//				@Override
+//				public void onClick(View arg0) {
+//					// TODO Auto-generated method stub
+//					CheckIntenetConnection();
+//
+//				}
+//			});
+//
+//
+//
+//		}
+//	}
+	// ------------------------------------------------------------------------------------
+	// ------------------------------------------------------------------------------------
+	// ------------------------------------------------------------------------------------
+	// ------------------------------------------------------------------------------------
+	// ------------------------------------------------------------------------------------
+	// ------------------------------------------------------------------------------------
+	// ------------------------------------------------------------------------------------
+
+}
