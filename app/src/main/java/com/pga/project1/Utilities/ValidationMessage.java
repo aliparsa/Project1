e4f22e19061eb24ddda2462a81bf8363b5a203f1
ashkan <ashkanpower@gmail.com>
2014-08-09 14:42:23 +0430

    login validation

---
+++ app/src/main/java/com/pga/project1/Utilities/ValidationMessage.java
@@ -0,0 +1,15 @@
+package com.pga.project1.Utilities;
+
+import java.util.List;
+
+/**
+ * Created by ashkan on 8/9/2014.
+ */
+public class ValidationMessage {
+
+
+    List<String> messages;
+
+
+
+}
--- app/src/main/java/com/pga/project1/fragment/FragmentLogin.java
+++ app/src/main/java/com/pga/project1/fragment/FragmentLogin.java
@@ -3,11 +3,15 @@ package com.pga.project1.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.method.CharacterPickerDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.pga.project1.R;
+import com.pga.project1.Utilities.ValidationMessage;
 
 /**
  * Created by ashkan on 8/9/2014.
@@ -24,6 +28,10 @@ public class FragmentLogin extends Fragment {
     //-----------------------------------------------------static fields}
 
     //{Fields-----------------------------------------------------
+
+    EditText txtUsername;
+    EditText txtPassword;
+    Button btnLogin;
 
     //-----------------------------------------------------Fields}
 
@@ -46,12 +54,57 @@ public class FragmentLogin extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        txtUsername = (EditText) getView().findViewById(R.id.etxt_fragmentLogin_username);
+        txtPassword = (EditText) getView().findViewById(R.id.etxt_fragmentLogin_password);
+
+        btnLogin = (Button) getView().findViewById(R.id.btn_fragmentLogin_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public ValidationMessage validationMessage;
+
+            @Override
+            public void onClick(View view) {
+
+                String username = txtUsername.getText().toString();
+                String password = txtUsername.getText().toString();
+
+                LoginValidation(username, password, validationMessage);
+
+                loginClicked();
+            }
+        });
+    }
+
+
+
 
     //-----------------------------------------------------override functions}
 
+
+
     //{Functions-----------------------------------------------------
 
+    private void loginClicked() {
+
+    }
+
     //-----------------------------------------------------Functions}
+
+
+
+    //validations-----------------------------------------------------
+
+    private boolean LoginValidation(String username, String password, ValidationMessage validationMessage) {
+        
+        return true;
+    }
+
+    //-----------------------------------------------------validations}
+
+
 
     //{static Functions-----------------------------------------------------
 
