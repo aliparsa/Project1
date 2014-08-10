dc365cba1869eb41f7eeacf0c130285b2ae3fdb3
ashkan <ashkanpower@gmail.com>
2014-08-10 10:50:16 +0430

    login validation

---
+++ app/src/main/java/com/pga/project1/Utilities/Webservice.java
@@ -0,0 +1,12 @@
+package com.pga.project1.Utilities;
+
+import com.pga.project1.Intefaces.CallBack;
+import com.pga.project1.Structures.Chart;
+
+/**
+ * Created by ashkan on 8/10/2014.
+ */
+public class Webservice {
+    public void getProjects(CallBack<Chart> callBack) {
+    }
+}
