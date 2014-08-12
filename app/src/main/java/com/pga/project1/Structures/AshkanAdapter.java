9ba06907dc0e54d26f16719096c3b014ebbed297
Pishgaman-Asia <android@pishgamanasia.com>
2014-08-12 09:18:14 +0430

    Stable Version

--- app/src/main/java/com/pga/project1/Structures/AshkanAdapter.java
+++ app/src/main/java/com/pga/project1/Structures/AshkanAdapter.java
@@ -10,15 +10,15 @@ import java.util.ArrayList;
 /**
  * Created by ashkan on 8/10/2014.
  */
-public class AshkanAdapter extends ArrayAdapter<ListViewRowItem> {
-
-    public AshkanAdapter(Context context, int resource, ArrayList<ListViewRowItem> listItem) {
-        super(context, resource, listItem);
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        return this.getItem(position).getView(this.getContext(), convertView);
-    }
-}
+//public class AshkanAdapter extends ArrayAdapter<ListViewRowItem> {
+//
+//    public AshkanAdapter(Context context, int resource, ArrayList<ListViewRowItem> listItem) {
+//        super(context, resource, listItem);
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//
+//        return this.getItem(position).getView(this.getContext(), convertView);
+//    }
+//}
--- app/src/main/java/com/pga/project1/Utilities/Webservice.java
+++ app/src/main/java/com/pga/project1/Utilities/Webservice.java
@@ -1,9 +1,11 @@
 package com.pga.project1.Utilities;
 
+import com.pga.project1.Asyncs.AsyncGetChildById;
 import com.pga.project1.Asyncs.AsyncLoad;
 import com.pga.project1.Asyncs.AsyncLoginByUserPass;
 import com.pga.project1.Intefaces.CallBackArraylist;
 import com.pga.project1.Intefaces.CallBackAsync;
+import com.pga.project1.Intefaces.CallBackGetChildOfId;
 import com.pga.project1.Intefaces.CallBackJSON;
 import com.pga.project1.Intefaces.CallBackLogin;
 import com.pga.project1.Structures.Chart;
@@ -28,10 +30,10 @@ public class Webservice {
 
     public static void getProjects(final CallBackArraylist<Chart> callBack) {
 
-        CallBackJSON  callback_json = new CallBackJSON() {
+        CallBackJSON callback_json = new CallBackJSON() {
             @Override
             public void onSuccess(JSONArray result) {
-                ArrayList<Chart> chartList =  Chart.getArrayFromJson(result);
+                ArrayList<Chart> chartList = Chart.getArrayFromJson(result);
                 callBack.onSuccess(chartList);
             }
 
@@ -42,15 +44,34 @@ public class Webservice {
 
                 );
             }
-        } ;
+        };
         new AsyncLoad(GET_PROJECTS_URL, callback_json).execute();
     }
 
-    public ArrayList<Chart> getChildesById(int id){
-        return null;
+    //-----------------------------------------------------------------------------
+    public static void GetChildOfID(final int id, final CallBackGetChildOfId callBackGetChildOfId) {
+
+        if (DataCache.get(id + "") != null) {
+
+            callBackGetChildOfId.onSuccess(Chart.getArrayFromJson(DataCache.get(id + "")));
+        } else {
+            new AsyncGetChildById(GET_PROJECTS_URL, id, new CallBackAsync<JSONArray>() {
+                @Override
+                public void onSuccess(JSONArray json) {
+
+                    DataCache.set(id + "", json);
+                    callBackGetChildOfId.onSuccess(Chart.getArrayFromJson(json));
+                }
+
+                @Override
+                public void onError(String json) {
+
+                }
+            }).execute();
+        }
     }
 
-
+    //------------------------------------------------------------------------
     public static void Login(String username, String password, final CallBackLogin callbackLogin) {
         new AsyncLoginByUserPass(LOGIN_URL, username, password, new CallBackAsync<JSONObject>() {
             @Override
@@ -69,13 +90,15 @@ public class Webservice {
             }
 
             @Override
-            public void onError(String error) {
+            public void onError(String jsonObject) {
 
             }
-
-
         }).execute();
 
     }
 
+    public ArrayList<Chart> getChildesById(int id) {
+        return null;
+    }
+    //-------------------------------------------------------------------------------
 }
