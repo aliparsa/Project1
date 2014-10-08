1d3a299020d0fbccde0b5dfe66c693b5abca59bf
ashkan <ashkanpower@gmail.com>
2014-10-08 10:18:14 +0330

    activity setting action bar

--- app/src/main/java/com/pga/project1/Activities/ShowImageActivity.java
+++ app/src/main/java/com/pga/project1/Activities/ShowImageActivity.java
@@ -4,9 +4,6 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -76,24 +73,7 @@ public class ShowImageActivity extends Activity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_show_image, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 //    private class GalleryImageAdapter extends BaseAdapter {
 //
