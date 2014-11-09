87827bd030020c192878df363521d566a2f37b42
ashkan <ashkanpower@gmail.com>
2014-11-09 10:28:07 +0330

    persian digits date picker

--- app/src/main/java/com/pga/project1/Viewes/ViewDateTimePickerPersian.java
+++ app/src/main/java/com/pga/project1/Viewes/ViewDateTimePickerPersian.java
@@ -168,10 +168,16 @@ public class ViewDateTimePickerPersian extends LinearLayout {
         hourPicker.setMaxValue(23);
 
         hourPicker.setDisplayedValues(new String[]{
+                "٠٠", "٠١", "٠٢", "٠٣", "٠٤", "٠٥", "٠٦", "٠٧", "٠٨", "٠٩",
+                "١٠", "١١", "١٢", "١٣", "١٤", "١٥", "١٦", "١٧", "١٨", "١٩",
+                "٢٠", "٢١", "٢٢", "٢٣",
+        });
+
+/*        hourPicker.setDisplayedValues(new String[]{
                 "00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
                 "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
                 "20", "21", "22", "23",
-        });
+        });*/
     }
 
     private void setMinutePicker() {
