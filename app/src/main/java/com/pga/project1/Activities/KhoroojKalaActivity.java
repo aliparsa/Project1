5b0d4a27dfaa71b88a938fbad9c2f1bb3b0e327e
ashkan ghodrat <ashkanpower@gmail.com>
2014-11-15 19:51:46 +0330

    vorod and khoroj activity action bar

--- app/src/main/java/com/pga/project1/Activities/KhoroojKalaActivity.java
+++ app/src/main/java/com/pga/project1/Activities/KhoroojKalaActivity.java
@@ -1,37 +1,67 @@
 package com.pga.project1.Activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.pga.project1.R;
+import com.pga.project1.Utilities.FontHelper;
+import com.pga.project1.Utilities.Fonts;
 
 public class KhoroojKalaActivity extends Activity {
+
+    private ImageView save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_khorooj_kala);
+
+        prepareActionBar();
+
     }
 
+    private void prepareActionBar() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.khorooj_kala, menu);
-        return true;
-    }
+        View customActionBar = getLayoutInflater().inflate(R.layout.actionbar_back, null);
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setCustomView(customActionBar);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        TextView title = (TextView) customActionBar.findViewById(R.id.ac_title);
+        FontHelper.SetFont(this, Fonts.MAIN_FONT, title, Typeface.BOLD);
+
+        title.setText("خروج کالا");
+
+        LinearLayout back = (LinearLayout) customActionBar.findViewById(R.id.ac_back_layout);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        save = (ImageView) customActionBar.findViewById(R.id.ac_action1);
+
+        save.setImageResource(R.drawable.ic_save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO save khoroj kala
+            }
+        });
+
+
     }
 }
